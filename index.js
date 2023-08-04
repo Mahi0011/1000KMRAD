25d0b10f4b36e4592b8fc3ed9fd6b3cd03921d64
MAHI AL AKHNAS <mahialakhnas@gmail.com>
2023-08-04 10:24:51 +0530

    shorting code

--- index.js
+++ index.js
@@ -1,224 +1,142 @@
-// const express = require('express')
-// const app = express()
-// const geolib = require('geolib')
-
-
-
-// let getdata  = () =>{
-//     return new Promise(async(resolve,reject)=>{
-//         let result = await fetch('https://ignite.zook.top/data.json')
-//         let data = result.json()
-//         resolve(data)
-//     })
-// }
-
-// let check =() =>{
-//   return new Promise(async(resolve,reject)=>{
-//     getdata()
-//     .then((data)=>data.map((x)=>{
-//       let res = {}
-//       res.name = x.fullName
-//       res.loc = x.address.geo
-//       return res
-//   }))
-//   .then( (x)=>{
-//     let obj ={}
-//     let fin = []
-//     let grp = 1
-//     let b=[]
-//     for(let i =0;i<x.length;i++){
-//      if(fin.includes(i))
-//       continue
-//      else{
-//       for(let j=i+1;j<x.length;j++)
-//             {
-//               if(fin.includes(j))
-//               continue
-//               else
-//               {
-//                 let res = geolib.isPointWithinRadius(x[i].loc,x[j].loc,1000000)
-//               if(res){
-//                 let dis = geolib.getDistance(x[i].loc,x[j].loc)
-//                 b.push({sid:j,name:x[j].name,dis})
-//                 fin.push(j)
-//               }
-//               }
-              
-//             }b.push({fid:i,name:x[i].name})
-//             obj['group'+grp] = b
-//             grp++
-//              b=[]
-//      }
-          
-//     }
-//     resolve(obj)
-    
-//   })
-//   })
-
-//      }
-
-//      let fun = (x) =>{
-//       let gr =[]
-//       let sm =[]
-//       let o = 0
-//       let h = x['group1'].length
-//       for (const [key, value] of Object.entries(x)) {
-//         // console.log(key,value.length)
-//         if(o<value.length){
-//           o = value.length
-//         gr=[]
-//           gr[0]=key
-//         }else if(value.length===o){
-//           gr.push(key)
-//         }if(value.length<h){
-//           h=value.length
-//           sm=[]
-//           sm[0]=key
-//         }else if(value.length===h){
-//           sm.push(key)
-//         }
-//       }
-//       let base = {
-//         group : Object.keys(x).length,
-//         biggroup : gr,
-//         bglen :x[gr[0]].length,
-//         smallgroup : sm,
-//         smlen:x[sm[0]].length,
-//       }
-//       return base
-//     }
-// let f = () =>{
-//   check()
-//   .then((x)=>{
-//     let res = fun(x)
-//     console.log(res)
-//   })
-// }
-// // f()
-
-// app.set('view engine','hbs')
-
-// app.get('/', async (req,res)=>{
-//   check()
-//   .then((x)=>{
-//     let s = fun(x)
-//     let base = fun(x)
-//     res.render('view',{data:x,base})
-// console.log(base)
-//   })
- 
-//   })
-  
-
-// app.listen(3000,()=>{
-//     console.log("server running on port 3000")
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const express = require('express');
 const app = express();
 const geolib = require('geolib');
 
-const getData = async () => {
+
+//=================RADIUS INITIALISING======================
+
+const radius = 1000;
+
+//================FETCHING DATA ============================
+
+const fetchData = async () => {
   const result = await fetch('https://ignite.zook.top/data.json');
   const data = await result.json();
   return data;
 };
 
-const groupData = async () => {
-  const data = await getData();
+//==================DESTRUCTURING DATA======================
 
-  const transformedData = data.map((x) => ({
-    name: x.fullName,
-    loc: x.address.geo,
+const groupData = async () => {
+  const data = await fetchData();
+  const transformedData = data.map((item) => ({
+    name: item.fullName,
+    location: item.address.geo,
   }));
 
-  let obj = {};
-  let fin = [];
-  let grp = 1;
-  let b = [];
+  let response = {};
+  let visitedIndices = [];
+  let groupCounter = 1;
+  let groupedLocations = [];
+
+//=======================GROUPING PEOPLE BASED ON RADIUS=========================
 
   for (let i = 0; i < transformedData.length; i++) {
-    if (fin.includes(i)) continue;
-
+    if (visitedIndices.includes(i)) continue;
     for (let j = i + 1; j < transformedData.length; j++) {
-      if (fin.includes(j)) continue;
-      let radius = 25000
-      const res = geolib.isPointWithinRadius(transformedData[i].loc, transformedData[j].loc, (radius*1000)/2);
+      if (visitedIndices.includes(j)) continue;
+      const res = geolib.isPointWithinRadius(transformedData[i].location, transformedData[j].location, radius * 1000);
       if (res) {
-        const dis = geolib.getDistance(transformedData[i].loc, transformedData[j].loc);
-        b.push({ sid: j, name: transformedData[j].name, dis });
-        fin.push(j);
+        groupedLocations.push({ id: j, name: transformedData[j].name, location: transformedData[j].location });
+        visitedIndices.push(j);
       }
     }
+    groupedLocations.push({ id: i, name: transformedData[i].name, location: transformedData[i].location });
+    let finalExcludedIds = [];
 
-    b.push({ fid: i, name: transformedData[i].name });
-    obj['group' + grp] = b;
-    grp++;
-    b = [];
+//====================SORTING GROUP SUCH THAT DISTANCE BETWEEN EACH PERSON IS LESS THAN THE GIVEN DISTANCE==========================
+
+    for (let m = 0; m < groupedLocations.length; m++) {
+      if (finalExcludedIds.includes(groupedLocations[m].id)) continue;
+      for (let n = 0; n < groupedLocations.length; n++) {
+        if (finalExcludedIds.includes(groupedLocations[n].id)) continue;
+        let distance = geolib.getDistance(groupedLocations[m].location, groupedLocations[n].location);
+        if (distance > radius * 1000) {
+          finalExcludedIds.push(groupedLocations[n].id);
+        }
+      }
+    }
+    finalExcludedIds.map((excludedId) => {
+      visitedIndices = visitedIndices.filter((id) => id !== excludedId);
+    });
+    finalExcludedIds.map((excludedId) => {
+      groupedLocations = groupedLocations.filter((item) => item.id !== excludedId);
+    });
+    response['group-' + groupCounter] = groupedLocations;
+    groupCounter++;
+    groupedLocations = [];
   }
-
-  return obj;
+  return response;
 };
 
-const getGroupSummary = (x) => {
-  let gr = [];
-  let sm = [];
-  let o = 0;
-  let h = x['group1'].length;
+//====================GETTING GROUP SUMMARY============================
+const getGroupSummary = (data) => {
+  let largest = [], smallest = [], largestSize = 0, smallestSize = Infinity;
 
-  for (const [key, value] of Object.entries(x)) {
-    if (o < value.length) {
-      o = value.length;
-      gr = [key];
-    } else if (value.length === o) {
-      gr.push(key);
-    }
-
-    if (value.length < h) {
-      h = value.length;
-      sm = [key];
-    } else if (value.length === h) {
-      sm.push(key);
-    }
+  for (const [key, value] of Object.entries(data)) {
+    if (value.length > largestSize) largestSize = value.length, largest = [key];
+    else if (value.length === largestSize) largest.push(key);
+    if (value.length < smallestSize) smallestSize = value.length, smallest = [key];
+    else if (value.length === smallestSize) smallest.push(key);
   }
 
   return {
-    group: Object.keys(x).length,
-    biggroup: gr,
-    bglen: x[gr[0]].length,
-    smallgroup: sm,
-    smlen: x[sm[0]].length,
+    groupCount: Object.keys(data).length,
+    largestGroups: largest,
+    largestGroupSize: data[largest[0]].length,
+    smallestGroups: smallest,
+    smallestGroupSize: data[smallest[0]].length,
   };
 };
+
+
+// const getGroupSummary = (data) => {
+//   let largestGroups = [];
+//   let smallestGroups = [];
+//   let largestSize = 0;
+//   let smallestSize = Infinity;
+
+//   for (const [key, value] of Object.entries(data)) {
+//     if (value.length > largestSize) {
+//       largestSize = value.length;
+//       largestGroups = [key];
+//     } else if (value.length === largestSize) {
+//       largestGroups.push(key);
+//     }
+
+//     if (value.length < smallestSize) {
+//       smallestSize = value.length;
+//       smallestGroups = [key];
+//     } else if (value.length === smallestSize) {
+//       smallestGroups.push(key);
+//     }
+//   }
+
+//   return {
+//     groupCount: Object.keys(data).length,
+//     largestGroups: largestGroups,
+//     largestGroupSize: data[largestGroups[0]].length,
+//     smallestGroups: smallestGroups,
+//     smallestGroupSize: data[smallestGroups[0]].length,
+//   };
+// };
+
+let start = async () => {
+  const data = await groupData();
+  const groupSummary = getGroupSummary(data);
+};
+start();
 
 app.set('view engine', 'hbs');
 
 app.get('/', async (req, res) => {
   try {
     const data = await groupData();
-    const base = getGroupSummary(data);
-    res.render('view', { data, base });
-    console.log(base);
+    const groupSummary = getGroupSummary(data);
+    res.render('view', { data, groupSummary });
   } catch (err) {
+    console.log(err)
     res.status(500).send('Internal Server Error');
   }
 });
